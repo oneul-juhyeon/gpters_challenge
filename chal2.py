8584bc35b1e26b20793c6c7224c7195181deee0b
oneul-juhyeon <143252945+oneul-juhyeon@users.noreply.github.com>
2024-04-22 21:51:42 +0900

    Update chal2.py

--- chal2.py
+++ chal2.py
@@ -223,7 +223,26 @@ def main():
         with col4:
             weekly_mission_button = st.button('주간미션')
 
+
         # 독서인증 결과 표시 (index=False로 설정하여 인덱스를 표시하지 않음)
         if daily_mission_button:
             messages.append(f"### 🔥 독서 파워가 가장 높은 멤버는? \n지금까지 가장 인증을 많이 한 멤버는 {top_users_str}입니다. 부자 되시겠군요?")
-            messages.append(f"### 💝 어제 독서인증을 성공한 멤버는?\n{yesterday}에 인증을 성공한 멤버는 {successful_
+            messages.append(f"### 💝 어제 독서인증을 성공한 멤버는?\n{yesterday}에 인증을 성공한 멤버는 {successful_users_yesterday_str}입니다. 어제도 정말 수고 하셨어요!")
+            
+            for message in messages:
+                st.markdown(message)
+                
+            # 표와 메시지 사이의 줄바꿈 추가
+            st.markdown("\n\n", unsafe_allow_html=True)
+            st.markdown("\n\n", unsafe_allow_html=True)
+        
+            # 전체 결과 보기
+            st.subheader("독서 인증 전체 결과 보기")
+        
+            # 결과 표시 (index=False로 설정하여 인덱스를 표시하지 않음)
+            st.dataframe(final_result_df.reset_index(drop=True))
+            
+            # 줄바꿈 추가
+            st.markdown("\n\n", unsafe_allow_html=True)
+            st.markdown("\n\n", unsafe_allow_html=True)
+            st.markdown("\n\n", unsafe_allow_html=True)
